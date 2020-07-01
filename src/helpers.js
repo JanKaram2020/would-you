1c8687371abe98fd9fffaadaa784d436e4dc4991
JanKaram2020 <65977048+JanKaram2020@users.noreply.github.com>
2020-07-01 20:15:21 +0200

    reformatted using prettier

--- src/helpers.js
+++ src/helpers.js
@@ -1,34 +1,57 @@
-import { questionsReceived, questionAdded, questionAnswered } from "./store/questions";
-import { usersReceived, questionAddedToUser, answerAddedToUser } from "./store/users";
-import {_getQuestions, _getUsers, _saveQuestion, _saveQuestionAnswer} from "./_DATA";
+import {
+  questionsReceived,
+  questionAdded,
+  questionAnswered,
+} from "./store/questions";
+import {
+  usersReceived,
+  questionAddedToUser,
+  answerAddedToUser,
+} from "./store/users";
+import {
+  _getQuestions,
+  _getUsers,
+  _saveQuestion,
+  _saveQuestionAnswer,
+} from "./_DATA";
 
 export function handleInitialData() {
   return (dispatch) => {
-    return Promise.all([_getUsers(), _getQuestions()]).then(
-        ([users, questions]) => ({
-            users,
-            questions,
-        })
-    ).then(({ users, questions }) => {
-      dispatch(questionsReceived(questions));
-      dispatch(usersReceived(users));
-    });
+    return Promise.all([_getUsers(), _getQuestions()])
+      .then(([users, questions]) => ({
+        users,
+        questions,
+      }))
+      .then(({ users, questions }) => {
+        dispatch(questionsReceived(questions));
+        dispatch(usersReceived(users));
+      });
   };
 }
 export function handleSaveQuestion(optionOneText, optionTwoText, author) {
   return (dispatch) => {
-    return _saveQuestion({ optionOneText:optionOneText, optionTwoText:optionTwoText, author:author }).then(
-        (question) => {
-          dispatch(questionAdded(question));
-          dispatch(questionAddedToUser(question));
-        }
-    );
+    return _saveQuestion({
+      optionOneText: optionOneText,
+      optionTwoText: optionTwoText,
+      author: author,
+    }).then((question) => {
+      dispatch(questionAdded(question));
+      dispatch(questionAddedToUser(question));
+    });
   };
 }
 export function handleAnswerQuestion(authUser, qid, answer) {
-    return dispatch => {
-        dispatch(answerAddedToUser({authUser:authUser, qid:qid, answer:answer}));
-        dispatch(questionAnswered({authUser:authUser, qid:qid, answer:answer}));
-        return _saveQuestionAnswer({authUser:authUser, qid:qid, answer:answer});
-    };
+  return (dispatch) => {
+    dispatch(
+      answerAddedToUser({ authUser: authUser, qid: qid, answer: answer })
+    );
+    dispatch(
+      questionAnswered({ authUser: authUser, qid: qid, answer: answer })
+    );
+    return _saveQuestionAnswer({
+      authUser: authUser,
+      qid: qid,
+      answer: answer,
+    });
+  };
 }
