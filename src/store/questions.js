b8eb9e32d64b38a334c1960b93f2cb4124620f0e
JanKaram2020 <65977048+JanKaram2020@users.noreply.github.com>
2020-07-02 08:39:07 +0200

    removed comments from the store

--- src/store/questions.js
+++ src/store/questions.js
@@ -13,24 +13,10 @@ const slice = createSlice({
     questionAdded: (questions, action) => {
       const {id} = action.payload;
       questions[id] = action.payload;
-      // return {
-      //   ...questions,
-      //   [action.payload.id]: action.payload,
-      // };
     },
     questionAnswered: (questions, action) => {
       const { authUser, qid, answer } = action.payload;
       questions[qid][answer].votes.concat(authUser);
-      // return {
-      //   ...questions,
-      //   [qid]: {
-      //     ...questions[qid],
-      //     [answer]: {
-      //       ...questions[qid][answer],
-      //       votes: questions[qid][answer].votes.concat(authUser),
-      //     },
-      //   },
-      // };
     },
   },
 });
--- src/store/users.js
+++ src/store/users.js
@@ -13,27 +13,10 @@ const slice = createSlice({
     answerAddedToUser: (users, action) => {
       const { authUser, qid, answer } = action.payload;
       users[authUser].answers[qid] = answer;
-      // return {
-      //   ...users,
-      //   [authUser]: {
-      //     ...users[authUser],
-      //     answers: {
-      //       ...users[authUser].answers,
-      //       [qid]: answer,
-      //     },
-      //   },
-      // };
     },
     questionAddedToUser: (users, action) => {
       const { author, id } = action.payload;
       users[author].questions.concat(id);
-      // return {
-      //   ...users,
-      //   [author]: {
-      //     ...users[author],
-      //     questions: users[author].questions.concat(id),
-      //   },
-      // };
     },
   },
 });
